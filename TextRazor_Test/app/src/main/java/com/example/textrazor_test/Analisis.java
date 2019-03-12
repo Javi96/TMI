6d53e93bcb267a2145c425d3a3fbd3a3202d6bdd
Aitorcay <aitorcay@ucm.es>
2019-03-12 17:54:04 +0100

    Consulta y muestra resultados por consola

---
+++ TextRazor_Test/app/src/main/java/com/example/textrazor_test/Analisis.java
@@ -0,0 +1,14 @@
+package com.example.textrazor_test;
+
+import com.textrazor.annotations.Entity;
+import com.textrazor.annotations.NounPhrase;
+import com.textrazor.annotations.Word;
+
+import java.util.ArrayList;
+
+public class Analisis {
+    ArrayList<Word> listaWords = new ArrayList<Word>();
+    ArrayList<NounPhrase> listaPhrases = new ArrayList<NounPhrase>();
+    ArrayList<Entity> listaEntities = new ArrayList<Entity>();
+
+}
