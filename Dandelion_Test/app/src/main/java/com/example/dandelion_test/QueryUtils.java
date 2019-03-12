a0e3dee1327cb8d8cce1d43019fdb34c6b949469
Aitorcay <aitorcay@ucm.es>
2019-03-12 17:36:03 +0100

    Añadir APIKey a "generaURL" en MainActivity

---
+++ Dandelion_Test/app/src/main/java/com/example/dandelion_test/QueryUtils.java
@@ -0,0 +1,167 @@
+package com.example.dandelion_test;
+
+import android.util.Log;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+/**
+ * Helper methods related to requesting and receiving earthquake data from USGS.
+ */
+public final class QueryUtils {
+
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+     /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
+
+
+    public static ArrayList<String> fetchEarthquakeData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (java.io.IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request", e);
+        }
+
+        // Extract relevant fields from the JSON response and create an {@link Event} object
+       ArrayList<String> entities = extractEntities(jsonResponse);
+
+        // Return the {@link Event}
+        return entities;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (java.net.MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws java.io.IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (java.net.HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (java.io.IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws java.io.IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    public static ArrayList<String> extractEntities(String jsonResponse) {
+
+        // Create an empty ArrayList that we can start adding earthquakes to
+        ArrayList<String> entities = new ArrayList<>();
+
+        // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+
+            JSONObject jsonRootObject = new JSONObject(jsonResponse);
+
+            //Get the instance of JSONArray that contains JSONObjects
+            JSONArray entityArray = jsonRootObject.optJSONArray("annotations");
+
+            //Iterate the jsonArray and print the info of JSONObjects
+            for(int i=0; i < entityArray.length(); i++){
+                JSONObject currentEntity = entityArray.getJSONObject(i);
+
+                String name = currentEntity.getString("spot");
+                ArrayList<String> types = new ArrayList<String>();
+                ArrayList<String> categories= new ArrayList<String>();
+                JSONArray typesURL = currentEntity.optJSONArray("types");
+                JSONArray categoriesURL = currentEntity.optJSONArray("categories");
+                for (int j=0; j < typesURL.length(); j++)
+                    types.add(typesURL.getString(j));
+                for (int j=0; j < categoriesURL.length(); j++)
+                    categories.add(categoriesURL.getString(j));
+
+                Entity ent = new Entity(name, types, categories);
+                entities.add(ent.toString());
+            }
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the Entities JSON results", e);
+        }
+
+        return entities;
+    }
+}
+
+
