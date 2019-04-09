524706b3ef7aeb99b3aa2b582644ce7ed2d95170
unknown <gabriese@ucm.es>
2019-04-10 00:58:49 +0200

    mejora en la configuración de flask

--- recipe_obtaining/flasktest.py
+++ recipe_obtaining/flasktest.py
@@ -5,6 +5,11 @@ from recipe_module import get_recipe
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def init():
+    return 'Active'
+
+
 @app.route('/recipe/<plate>', methods=['GET'])
 def hello(plate):
     #return jsonify({'ip':'88.0.109.140','msg':'Welcome to Salk API','status':True, 'neural-network':connected})
@@ -12,3 +17,4 @@ def hello(plate):
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5555) 
+	  
